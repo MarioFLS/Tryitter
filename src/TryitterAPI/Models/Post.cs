e86b597f0d67df044065ead66e8dcb93bd99352f
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-29 16:08:00 -0300

    feat: model Post

---
+++ src/TryitterAPI/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TryitterAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 palavras")]
+        public string Text { get; set; }
+        public Student Student { get; set; }
+
+        public Post(int id, string title, string text, Student student)
+        {
+            Id = id;
+            Title = title;
+            Text = text;
+            Student = student;
+        }
+    }
+}
