854a4799ca5bcfc625187836da5e0f287351bb90
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-29 16:08:09 -0300

    feat: Model Student

---
+++ src/TryitterAPI/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TryitterAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [Column("Post_Id")]
+        [ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public ICollection<Post> Posts { get; set; }
+
+        public Student(int id, string name, string email, string password, int postId, ICollection<Post> posts)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            PostId = postId;
+            Posts = posts;
+        }
+    }
+}
