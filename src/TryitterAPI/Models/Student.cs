df3eb361c66d87350f0f511c0ee25444568a0e83
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 17:31:21 -0300

    feat: Validações da Model de estudante

--- src/TryitterAPI/Models/Student.cs
+++ src/TryitterAPI/Models/Student.cs
@@ -1,17 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace TryitterAPI.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [MinLength(3, ErrorMessage = "O seu nome deve ter no minimo 3 caracteres")]
         public string Name { get; set; } = default!;
 
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Digite um email válido")]
         public string Email { get; set; } = default!;
+        
+        [MinLength(5, ErrorMessage = "O senha deve ter deve ter no minimo 3 caracteres")]
         public string Password { get; set; } = default!;
-        public List<Post> Posts { get; set; } = default!;
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
 
     }
 }
