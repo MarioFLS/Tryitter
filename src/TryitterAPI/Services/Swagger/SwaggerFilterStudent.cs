75177b80c3fbd015c6ad537c6f7b1a83bf09fe54
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 17:31:48 -0300

    feat: Filtro pro Swagger de Id e Post

---
+++ src/TryitterAPI/Services/Swagger/SwaggerFilterStudent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace TryitterAPI.Services.Swagger
+{
+    public class SwaggerFilterStudent : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            schema.Properties.Remove("id");
+            schema.Properties.Remove("posts");
+        }
+    }
+}
