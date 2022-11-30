0355dc1fcba118b8b6cea10f6a5a231cb6865ec0
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-29 21:53:57 -0300

    feat: Gerador de Token

---
+++ src/TryitterAPI/Models/Auth/TokenGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TryitterAPI.Models.Auth
+{
+    public class TokenGenerator
+    {
+        public const string Secret = "2d74025e7bcf058897d8daaa99ae99b5";
+        public string Generate()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret)),
+                    SecurityAlgorithms.HmacSha256Signature
+                    ),
+                Expires = DateTime.Now.AddDays(1)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
