d8ca8029b9d77519792494cb042549249f3b48c0
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 16:21:56 -0300

    feat: Adicionando scopo dos repositorios

--- src/TryitterAPI/Program.cs
+++ src/TryitterAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TryitterContext>();
+builder.Services.AddScoped<ITryitterRepository, TryitterRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
