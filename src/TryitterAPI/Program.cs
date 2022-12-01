6fe928b74ae7930be0287354c019edc303ae961d
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 22:34:01 -0300

    feat: configurações do swagger

--- src/TryitterAPI/Program.cs
+++ src/TryitterAPI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi.Models;
 using TryitterAPI.Repository;
+using TryitterAPI.Services.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,17 @@ builder.Services.AddDbContext<TryitterContext>();
 builder.Services.AddScoped<ITryitterRepository, TryitterRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Tryitter",
+            Description = "Uma API de uma rede social chamada: Tryitter, talvez um pouco baseada no Twitter. Mas pode ser coincidência",
+        });
+
+        options.SchemaFilter<SwaggerFilterStudent>();
+    });
 
 var app = builder.Build();
 
@@ -19,6 +31,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "docs";
+
+});
+
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
 
 app.UseHttpsRedirection();
 
