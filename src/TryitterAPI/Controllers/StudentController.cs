9f941de7a542fe32a742877f790caa7886a36fa5
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 22:32:07 -0300

    feat: Rotas pre criação e logar como estudante

---
+++ src/TryitterAPI/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TryitterAPI.Models;
+using TryitterAPI.Models.Entities;
+using TryitterAPI.Repository;
+
+namespace TryitterAPI.Controllers
+{
+
+    [Route("student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ITryitterRepository _twitterRepository;
+        private Entities Entities { get; set; } = new();
+
+        public StudentController(ITryitterRepository twitterRepository)
+        {
+            _twitterRepository = twitterRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] Student student)
+        {
+            string token = _twitterRepository.CreateStudent(student);
+            if (token.IsNullOrEmpty())
+            {
+                return Problem("Já existe usuário com esse email", default, 400);
+            }
+            return StatusCode(201, new Entities.TokenText(token));
+        }
+
+        [HttpPost("/login")]
+        public IActionResult StudentLogin([FromBody] Entities.Login login)
+        {
+            string token = _twitterRepository.StudentLogin(login);
+            if(token.IsNullOrEmpty())
+            {
+                return Problem("Não existe Usuário com esse email", default, 400 );
+            }
+            return StatusCode(201, new Entities.TokenText(token));
+
+        }
+    }
+}
