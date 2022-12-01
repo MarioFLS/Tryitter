928d7e895b566cee647a399fe60b0d767c7c4554
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-01 15:41:18 -0300

    feat: Add validações de Model

--- src/TryitterAPI/Models/Entities/Entities.cs
+++ src/TryitterAPI/Models/Entities/Entities.cs
@@ -1,11 +1,16 @@
-﻿namespace TryitterAPI.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TryitterAPI.Models.Entities
 {
     public class Entities
     {
 
         public struct Login
         {
+            [DataType(DataType.EmailAddress)]
+            [EmailAddress(ErrorMessage = "Digite um email válido")]
             public string Email { get; set; }
+            [MinLength(5, ErrorMessage = "A senha deve ter deve ter no minimo 3 caracteres")]
             public string Password { get; set; }
 
             public Login(string email, string password)
