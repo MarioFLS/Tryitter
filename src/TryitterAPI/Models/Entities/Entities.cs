7639592f69c83d58c0d4fd20e306cae902e2cb37
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-30 22:31:06 -0300

    feat: entidades pra parametro

---
+++ src/TryitterAPI/Models/Entities/Entities.cs
@@ -0,0 +1,27 @@
+﻿namespace TryitterAPI.Models.Entities
+{
+    public class Entities
+    {
+        public struct TokenText
+        {
+            public string Token { get; set; }
+
+            public TokenText(string token)
+            {
+                Token = token;
+            }
+        }
+
+        public struct Login
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+
+            public Login(string email, string password)
+            {
+                Email = email;
+                Password = password;
+            }
+        }
+    }
+}
