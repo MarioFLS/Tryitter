c599d91becc9ad1cf90f8fc2a4506babc822192b
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-01 23:23:38 -0300

    feat: validação da Model

--- src/TryitterAPI/Models/Post.cs
+++ src/TryitterAPI/Models/Post.cs
@@ -10,8 +10,9 @@ namespace TryitterAPI.Models
         [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 palavras")]
         public string Text { get; set; } = default!;
 
+        [Range(1, 1, ErrorMessage = "Digite um id válido")]
         public int StudentId { get; set; }
 
-        public List<Image> Images { get; set; } = default!;
+        public List<Image>? Images { get; set; } = new List<Image>();
     }
 }
