3c796304e48ad1f724245b2bf7e973c45bedab26
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-01 23:23:17 -0300

    feat: Vlaidações da Model

--- src/TryitterAPI/Models/Image.cs
+++ src/TryitterAPI/Models/Image.cs
@@ -1,9 +1,16 @@
-﻿namespace TryitterAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TryitterAPI.Models
 {
     public class Image
     {
         public int Id { get; set; }
+
+        [DataType(DataType.Url)]
         public string? Link { get; set; }
+
+        [Range(1, 1, ErrorMessage = "Digite um id válido")]
+        public int PostId { get; set; }
 
     }
 }
