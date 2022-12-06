a4a5d532e0df620d8bb579b12245b814b44b0338
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-05 22:28:48 -0300

    feat: Criação de exceção para token inválido

---
+++ src/TryitterAPI/Services/Exception/InvalidTokenException.cs
@@ -0,0 +1,19 @@
+﻿namespace TryitterAPI.Services.Exception
+{
+    using System;
+
+    [Serializable]
+    public class InvalidTokenException : Exception
+    {
+        public InvalidTokenException() : base()
+        {
+
+        }
+        public InvalidTokenException(string messagem) : base(messagem)
+        {
+        }
+
+        public InvalidTokenException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
