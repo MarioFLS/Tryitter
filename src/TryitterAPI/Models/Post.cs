96a6eeb211c4272bcbe7a46aa17cb5c0c0da05ca
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-05 23:36:04 -0300

    feat: Validações de model

--- src/TryitterAPI/Models/Post.cs
+++ src/TryitterAPI/Models/Post.cs
@@ -4,12 +4,18 @@ namespace TryitterAPI.Models
 {
     public class Post
     {
-        public int Id { get; set; }
-        public string Title { get; set; } = default!;
+        public int Id { get; set;  }
 
-        [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 palavras")]
-        public string Text { get; set; } = default!;
+        [Required(ErrorMessage = "O titulo é obrigatório")]
+        [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 5 caracteres")]
+        public string Title { get; set; } = "";
 
+        [Required(ErrorMessage = "O texto é obrigatório")]
+        [MinLength(5, ErrorMessage = "O texto deve ter no mínimo 5 caracteres")]
+        [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 caracteres")]
+        public string Text { get; set; } = "";
+
+        [Required(ErrorMessage = "O id do estudante é obrigatório")]
         [Range(1, 1, ErrorMessage = "Digite um id válido")]
         public int StudentId { get; set; }
 
