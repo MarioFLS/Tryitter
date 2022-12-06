9e191243887af5f1a776a795a29ee9e2240079cd
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-05 23:24:02 -0300

    feat: Controller adicionar post

---
+++ src/TryitterAPI/Controllers/PostController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TryitterAPI.Models;
+using TryitterAPI.Repository;
+using TryitterAPI.Services.Exception;
+
+namespace TryitterAPI.Controllers
+{
+    [ApiController]
+    [Route("post")]
+    public class PostController : ControllerBase
+    {
+        private readonly ITryitterRepository _twitterRepository;
+
+        public PostController(ITryitterRepository twitterRepository)
+        {
+            _twitterRepository = twitterRepository;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreatePost([FromBody] Post post)
+        {
+            try
+            {
+
+
+                string? token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                if (token == null)
+                {
+                    throw new InvalidTokenException("Token Inválido");
+                }
+
+                var studentId = Convert.ToInt32(User?.Claims.First(claim => claim.Type == "id").Value);
+                Post newPost = new() { StudentId = studentId, Images = post.Images, Text = post.Text, Title = post.Title };
+
+                _twitterRepository.AddPost(newPost, token);
+                
+                return CreatedAtAction(nameof(CreatePost), new { id = studentId }, newPost);
+
+            }
+            catch (InvalidTokenException e)
+            {
+
+                return StatusCode(401, new { message = e.Message });
+            }
+
+        }
+    }
+}
