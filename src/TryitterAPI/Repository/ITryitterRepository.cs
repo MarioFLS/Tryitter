1189601ade43fb476cdca9864e319f1983e7da86
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-07 10:45:24 -0300

    feat: Novas função na interface do repositório

--- src/TryitterAPI/Repository/ITryitterRepository.cs
+++ src/TryitterAPI/Repository/ITryitterRepository.cs
@@ -7,7 +7,14 @@ namespace TryitterAPI.Repository
     {
         string CreateStudent(Student student);
         string StudentLogin(Entities.Login login);
-
         void AddPost(Post post);
+
+        Student? GetStudent(int id);
+        Post? GetPost(int id);
+        void EditStudent(Student student, Entities.UpdateStudent updateStudent);
+        void RemoveStudent(int id);
+
+        void EditPost(Post post, Entities.UpdatePost updatePost);
+        void RemovePost(int id);
     }
 }
