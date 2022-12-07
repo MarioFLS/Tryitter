e5b99f8ce201d28eb6450d5cf6272187b763d56a
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-07 10:45:42 -0300

    feat: Entidade de atualizar post e estudante

--- src/TryitterAPI/Models/Entities/Entities.cs
+++ src/TryitterAPI/Models/Entities/Entities.cs
@@ -19,5 +19,40 @@ namespace TryitterAPI.Models.Entities
                 Password = password;
             }
         }
+
+        public struct UpdateStudent
+        {
+            [MinLength(3, ErrorMessage = "O nome deve ter no minimo 3 caracteres")]
+            public string? Name { get; set; }
+
+            [MinLength(5, ErrorMessage = "A senha deve ter deve ter no minimo 3 caracteres")]
+            public string? Password { get; set; }
+
+            public UpdateStudent(string name, string password)
+            {
+                Name = name;
+                Password = password;
+            }
+        }
+
+        public struct UpdatePost
+        {
+
+            [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 5 caracteres")]
+            public string? Title { get; set; } = "";
+
+            [MinLength(5, ErrorMessage = "O texto deve ter no mínimo 5 caracteres")]
+            [MaxLength(300, ErrorMessage = "O texto pode ter no máximo 300 caracteres")]
+            public string? Text { get; set; } = "";
+
+            public List<Image>? Images { get; set; } = new List<Image>();
+
+            public UpdatePost(string? title, string? text, List<Image>? images)
+            {
+                Title = title;
+                Text = text;
+                Images = images;
+            }
+        }
     }
 }
