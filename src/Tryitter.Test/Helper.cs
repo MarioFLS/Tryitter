a8cf4723af0871c28538635c079f0e555c79c596
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-08 19:36:07 -0300

    test: Criação de usuário

---
+++ src/Tryitter.Test/Helper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TryitterAPI.Models;
+using TryitterAPI.Repository;
+
+namespace Tryitter.Test
+{
+    public class Helper
+    {
+        public static TryitterContext GetContextInstanceForTests(string inMemoryDbName)
+        {
+            var contextOptins = new DbContextOptionsBuilder<TryitterContext>()
+                .UseInMemoryDatabase(inMemoryDbName)
+                .Options;
+
+            var context = new TryitterContext(contextOptins);
+            Console.WriteLine(context);
+            
+            context.Students.AddRange(GetStudentListForTests());
+            context.SaveChanges();
+
+            context.Post.AddRange(GetSPostListForTests());
+            context.SaveChanges();
+
+
+            return context;
+        }
+
+        public static List<Student> GetStudentListForTests() =>
+        new() {
+            new Student {
+                Id = 1,
+                Name =  "Mario",
+                Email = "email@teste.com",
+                Password = "123456789"
+            },
+            new Student {
+                Id = 2,
+                Name =  "Fernando",
+                Email = "emailFernando@teste.com",
+                Password = "123456789"
+            },
+            new Student {
+                Id = 3,
+                Name =  "Jorge",
+                Email = "emailJorge@teste.com",
+                Password = "123456789"
+            },
+        };
+
+        public static List<Post> GetSPostListForTests() =>
+        new() {
+            new Post {
+                Id = 1,
+                Title =  "Titulo claramente bem pensado",
+                Text = "Texto extremamente explicativo",
+                StudentId = 1,
+            },
+            new Post {
+                Id = 2,
+                Title =  "Titulo claramente muito deveras bem pensado",
+                Text = "Texto extremamente explicativo sobre a explicação",
+                StudentId = 3,
+            },
+            new Post {
+                Id = 3,
+                Title =  "Titulo desesperado por criatividade",
+                Text = "Texto extremamente (não) criativo",
+                StudentId = 2,
+            },
+            new Post {
+                Id = 4,
+                Title =  "Titulo final",
+                Text = "Ultima Postagem",
+                StudentId = 2,
+            },
+        };
+    }
+}
---
+++ src/Tryitter.Test/TryitterRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using TryitterAPI.Models;
+using TryitterAPI.Repository;
+
+namespace Tryitter.Test
+{
+    public class TryitterRepositoryTest
+    {
+
+        [Trait("Estudante", "1 - Endpoint para criar novo Estudante")]
+        [Theory(DisplayName = "Criar um novo estudante e retorna um token")]
+        [MemberData(nameof(TestCreateNewStudentData))]
+        public void TestCreateNewStudent(TryitterContext context, string name, string email, string password)
+        {
+            TryitterRepository? _tryitterRepository = new(context);
+            Student student = new() { Name = name, Email = email, Password = password };
+            var token = _tryitterRepository.CreateStudent(student);
+
+            token.Should().NotBeNull();
+            Student response = context.Students.Where(s => s.Email == email).FirstOrDefault()!;
+            response.Email.Should().Be(email);
+
+            var responseFail = _tryitterRepository.CreateStudent(student);
+            responseFail.Should().BeNull();
+
+        }
+        public readonly static TheoryData<TryitterContext, string, string, string> TestCreateNewStudentData =
+        new()
+        {
+            {
+                Helper.GetContextInstanceForTests("TestCreateNewStudent"),
+                "Carlos",
+                "emailDeverasTEstado@test.com",
+                "12345698"
+            },
+        };
+    }
+}
