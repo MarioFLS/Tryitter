288d4b118d3812d58f84f81d745b37a5ea4ad828
MarioFLS <mariofernandolsantos@gmail.com>
2022-12-08 19:46:31 -0300

    test: Teste de login

--- src/Tryitter.Test/TryitterRepositoryTest.cs
+++ src/Tryitter.Test/TryitterRepositoryTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
 using TryitterAPI.Models;
+using TryitterAPI.Models.Entities;
 using TryitterAPI.Repository;
 
 namespace Tryitter.Test
@@ -34,6 +35,34 @@ namespace Tryitter.Test
                 "Carlos",
                 "emailDeverasTEstado@test.com",
                 "12345698"
+            },
+        };
+
+        [Trait("Estudante", "2 - Endpoint para logar como Estudante")]
+        [Theory(DisplayName = "Gera um Token apartir do Login")]
+        [MemberData(nameof(TestLoginStudentData))]
+        public void TestLoginStudent(TryitterContext context, string email, string password, string fakeEmail)
+        {
+            TryitterRepository? _tryitterRepository = new(context);
+            Entities.Login login = new() { Email = email, Password = password };
+            var token = _tryitterRepository.StudentLogin(login);
+
+            token.Should().NotBeNull();
+
+            Entities.Login fakeLogin = new() { Email = fakeEmail, Password = password };
+            var response = _tryitterRepository.StudentLogin(fakeLogin);
+            response.Should().BeNull();
+
+
+        }
+        public readonly static TheoryData<TryitterContext, string, string, string> TestLoginStudentData =
+        new()
+        {
+            {
+                Helper.GetContextInstanceForTests("TestLoginStudent"),
+                "emailFernando@teste.com",
+                "123456789",
+                "emailDeverasTEstado@test.com"
             },
         };
     }
